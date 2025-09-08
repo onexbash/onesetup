5ac35925324dd44adeb11463e9dac14f28cae7e4
onexbash <fabian@schlegel.one>
2025-09-08 21:19:48 +0200

    chore: update entrypoint script

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
-# Debug
-echo -e "ONESETUP_DIR: $ONESETUP_DIR \n ONESETUP_REPO: $ONESETUP_REPO DOTFILES_DIR: $DOTFILES_DIR \n DOTFILES_REPO: $DOTFILES_REPO"
-# Run the Ansible playbook once initially
-if [ -f "main.yml" ]; then
-  echo "Running the complete ansible-playbook..."
-  /usr/bin/ansible-playbook --ask-vault-password "main.yml"
-else
-  echo "Warning: main.yml not found. Skipping initial playbook run."
-fi
+function main() {
+  # Run the Ansible playbook once initially
+  if [ -f "main.yml" ]; then
+    echo "Running the complete ansible-playbook..."
+    /usr/bin/ansible-playbook --ask-vault-password "main.yml"
+  else
+    echo "Warning: main.yml not found. Skipping initial playbook run."
+  fi
+  
+  # Drop into an interactive shell for more commands
+  echo "Initial playbook execution completed. Starting interactive shell..."
+  exec /bin/bash
+}
 
-# Drop into an interactive shell for more commands
-echo "Initial playbook execution completed. Starting interactive shell..."
-exec /bin/bash
+# Debug
+function debug() {
+  echo -e "ONESETUP_DIR: $ONESETUP_DIR"
+  echo -e "ONESETUP_REPO: $ONESETUP_REPO"
+  echo -e "DOTFILES_DIR: $DOTFILES_DIR"
+  echo -e "DOTFILES_REPO: $DOTFILES_REPO"
+}
+
+main && debug
