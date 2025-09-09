56db50829f9f6715aa1c01378f2968515f3c3a78
onexbash <fabian@schlegel.one>
2025-09-09 12:50:46 +0200

    fix: auto-installation of ansible-galaxy modules

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 function main() {
+  # Install Modules from Ansible Galaxy
+  if [ -f "requirements.yml" ]; then
+    ansible-galaxy role install -r "requirements.yml"
+  else
+    echo "WARN: requirements.yml not found. Nothing will be installed from Ansible Galaxy."
+  fi
   # Run the Ansible playbook once initially
   if [ -f "main.yml" ]; then
-    echo "Running the complete ansible-playbook..."
-    /usr/bin/ansible-playbook --ask-vault-password "main.yml"
+    ansible-playbook --ask-vault-password "main.yml"
   else
-    echo "Warning: main.yml not found. Skipping initial playbook run."
+    echo "ERROR: main.yml not found. Skipping initial playbook run."
   fi
-  
-  # Drop into an interactive shell for more commands
-  echo "Initial playbook execution completed. Starting interactive shell..."
   exec /bin/bash
 }
 
