f905ee5b38a5fc72cb4af33a8be46bbaccfe24cb
onexbash <fabian@schlegel.one>
2025-10-08 17:43:15 +0200

    fix: helper script for bash shell

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+# (1) load_colors
 function load_colors() {
-  # -- DEFINE ANSI CODES -- #
-  local -A colorcodes
-  colorcodes=(
+  # Define Ansi Color Codes
+  local -A colors
+  colors=(
     ["black"]="0"
-    ["red"]="1" 
+    ["red"]="1"
     ["green"]="2"
     ["yellow"]="3"
     ["blue"]="4"
@@ -12,86 +14,80 @@ function load_colors() {
     ["white"]="7"
     ["gray"]="8"
   )
+  # Define Ansi Control Codes
+  local -A controls
+  controls=(
+    ["foreground"]="38"
+    ["background"]="48"
+    ["colorspace"]="5"
+  )
 
-  # Control Codes
-  local foreground="38"
-  local background="48"
-  local colorspace="5" # 5) 256 Colors | 2) RGB Colors
-  
-  # -- CONSTRUCT ANSI ESCAPE SEQUENCE -- #
+  # Construct Ansi Escape Sequence
   get_ansi_sequence() {
-    local type="$1"    # "fg" or "bg"
-    local color="$2"   # color name from colorcodes array
-    local code_type
-
-    if [[ "$type" == "fg" ]]; then
-      code_type="$foreground"
-    elif [[ "$type" == "bg" ]]; then
-      code_type="$background"
-    else
-      code_type="$type"
-    fi
-    
-    # return constructed ansi escape sequence
-    echo "\e[${code_type};${colorspace};${colorcodes[$color]}m"
+    local type="$1"  # "fg" or "bg"
+    local color="$2" # color name from colors array
+    echo "\e[${controls[$type]};${controls["colorspace"]};${colors[$color]}m"
   }
- 
-  # -- EXPORT VARIABLES -- #
-  # Get array keys in a compatible way
-  local color_keys
-  if [[ -n "$ZSH_VERSION" ]]; then
-    # Zsh: get keys from associative array
-    color_keys=("${(@k)colorcodes}")
-  else
-    # Bash: get keys from associative array
-    color_keys=("${!colorcodes[@]}")
-  fi
 
-  # Export Color Variables
-  for color in "${color_keys[@]}"; do
-    # Foreground Colors
-    # usage: `echo -e "${FG_BLUE}blue text"`
-    export "FG_${(U)color}"="$(get_ansi_sequence "fg" "$color")"
-    # Background Colors
-    # usage: `echo -e "${BG_BLUE}blue background"`
-    export "BG_${(U)color}"="$(get_ansi_sequence "bg" "$color")"
-  done 
+  # Construct & Export Color Variables
+  for color in "${!colors[@]}"; do
+    # Format Color Name to uppercase
+    local color_fmt
+    color_fmt=$(echo "$color" | tr '[:lower:]' '[:upper:]')
+    # Export Foreground Colors - usage: `echo -e "${FG_RED}red text"`
+    export "FG_${color_fmt}"="$(get_ansi_sequence "foreground" "$color")"
+    # Export Background Colors - usage: `echo -e "${BG_RED}red background"`
+    export "BG_${color_fmt}"="$(get_ansi_sequence "background" "$color")"
+  done
 }
 
+# (2) Load Font Style Variables
 function load_styles() {
-  # Export Style Variables
-  export S_RESET="\e[0m"
-  export S_BOLD="\e[1m"
+  # Define Variables
+  S_RESET="\e[0m"
+  S_BOLD="\e[1m"
+  # Export Variables
+  export S_RESET
+  export S_BOLD
 }
 
-# Load Prompt Style Variables
+# (3) Load Prompt Variables
 function load_prompts() {
+  # Define Variables
   # ok
-  export I_OK="${FG_BLACK}[${FG_GREEN}  OK  ${FG_BLACK}] ${S_RESET}"       
+  I_OK="${FG_BLACK}[${FG_GREEN}  OK  ${FG_BLACK}] ${S_RESET}"
   # warning
-  export I_WARN="${FG_BLACK}[${FG_YELLOW} WARNING ${FG_BLACK}] ${S_RESET}" 
+  I_WARN="${FG_BLACK}[${FG_YELLOW} WARNING ${FG_BLACK}] ${S_RESET}"
   # error
-  export I_ERR="${FG_BLACK}[${FG_RED} ERROR ${FG_BLACK}] ${S_RESET}"    
+  I_ERR="${FG_BLACK}[${FG_RED} ERROR ${FG_BLACK}] ${S_RESET}"
   # info
-  export I_INFO="${FG_BLACK}[${FG_MAGENTA} INFO ${FG_BLACK}] ${S_RESET}"    
+  I_INFO="${FG_BLACK}[${FG_MAGENTA} INFO ${FG_BLACK}] ${S_RESET}"
   # do
-  export I_DO="${FG_BLACK}[${FG_MAGENTA}  ...  ${FG_BLACK}] ${S_RESET}"     
+  I_DO="${FG_BLACK}[${FG_MAGENTA}  ...  ${FG_BLACK}] ${S_RESET}"
   # ask user for anything
-  export I_ASK="${FG_BLACK}[${FG_BLUE} ? ${FG_BLACK}] ${S_RESET}"          
+  I_ASK="${FG_BLACK}[${FG_BLUE} ? ${FG_BLACK}] ${S_RESET}"
   # ask user for yes or no
-  export I_ASK_YN="${FG_BLACK}[${FG_BLUE} [Y/N] ${FG_BLACK}] ${S_RESET}"   
+  I_ASK_YN="${FG_BLACK}[${FG_BLUE} [Y/N] ${FG_BLACK}] ${S_RESET}"
+  # Export Variables
+  export I_OK
+  export I_WARN
+  export I_ERR
+  export I_INFO
+  export I_DO
+  export I_ASK
+  export I_ASK_YN
 }
 
-# -- SET SCRIPT MODES -- #
+# (4) Set Shell-Script Modes
 function set_modes() {
   # Exit on error & pipe failures
   set -eo pipefail
   # Prompt whether script should run in debug-mode when $TOGGLE_SCRIPT_DEBUG_MODE env var is not set.
   if [[ -z $TOGGLE_SCRIPT_DEBUG_MODE ]]; then
     read -p "$(echo -e "${I_ASK_YN}Run Script in Debug Mode? ")" -r answer
-      case "$answer" in
-        [Yy]) TOGGLE_SCRIPT_DEBUG_MODE=1;;
-        *)    TOGGLE_SCRIPT_DEBUG_MODE=0;;
+    case "$answer" in
+    [Yy]) TOGGLE_SCRIPT_DEBUG_MODE=1 ;;
+    *) TOGGLE_SCRIPT_DEBUG_MODE=0 ;;
     esac
     if [[ $TOGGLE_SCRIPT_DEBUG_MODE -eq 1 ]]; then
       set -x && echo -e "${I_OK}Running Script in Debug Mode"
@@ -99,12 +95,19 @@ function set_modes() {
   fi
 }
 
-# -- GET DYNAMIC PATHS -- #
+# (4) Set Shell-Script Modes
 function get_paths() {
-  export THIS_FILE=$(realpath "$0")
-  export THIS_DIR=$(dirname "$(realpath "$0")")
-  if git rev-parse --git-dir > /dev/null 2>&1; then
-    export REPO_ROOT="$(git rev-parse --show-toplevel)"
+  THIS_FILE=$(realpath "$0")
+  THIS_DIR=$(dirname "$(realpath "$0")")
+  REPO_ROOT="$(git rev-parse --show-toplevel)"
+  # Export Default Path Variables
+  export THIS_FILE
+  export THIS_DIR
+  # Export Git Path Variables
+  if git rev-parse --git-dir >/dev/null 2>&1; then
+    export REPO_ROOT
+  else
+    echo -e "${I_WARN}The REPO_ROOT variable can only be detected inside of a git repository."
   fi
 }
 
@@ -118,10 +121,10 @@ function load_env_file() {
     return 1
   fi
   # Counter for provided, existing & missing env file paths
-  local count_args=0 # overall .env file paths passed to the function
+  local count_args=0     # overall .env file paths passed to the function
   local count_existing=0 # existing .env file paths
-  local count_missing=0 # non-existing .env file paths
-  # Source env files 
+  local count_missing=0  # non-existing .env file paths
+  # Source env files
   for env_file in "$@"; do
     ((count_args++))
     if [ -f "$env_file" ]; then
@@ -132,8 +135,8 @@ function load_env_file() {
     fi
   done
   echo -e "${I_INFO}You passed ${FG_BLUE}$count_args${S_RESET} .env file where ${FG_GREEN}$count_existing${S_RESET} exist and ${FG_RED}$count_missing${S_RESET} don't exist."
-  
-  # Return exit code
+
+  # Return exit-code
   if [ $count_missing -gt 0 ]; then
     return 1 # failure
   else
@@ -144,9 +147,9 @@ function load_env_file() {
 # -- DETECT OPERATING SYSTEM -- #
 function detect_os() {
   case "$(uname -s)" in
-    Linux*)  echo "linux";;
-    Darwin*) echo "macos";;
-    *)       echo "unknown";;
+  Linux*) echo "linux" ;;
+  Darwin*) echo "macos" ;;
+  *) echo "unknown" ;;
   esac
 }
 
